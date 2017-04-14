02cd7d60187143c358e02a5c693455d40006be5b
a.shafran <shopen21@gmail.com>
2017-04-14 15:58:02 +0300

    fixed build issues

--- src/app/core/hero.service.provider.ts
+++ src/app/core/hero.service.provider.ts
@@ -1,15 +1,15 @@
-import { Http } from '@angular/http';
+import {Http} from '@angular/http';
 
-import { HeroService } from './hero.service';
-import { Logger } from './logger.service';
-import { FactoryProvider } from '@angular/core';
+import {HeroService} from './hero.service';
+import {Logger} from './logger.service';
+import {FactoryProvider} from '@angular/core';
 
-export const heroServiceFactory = (http: Http, logger: Logger) => {
-    return new HeroService(http, logger, false);
-};
+export function heroServiceFactory(http: Http, logger: Logger): HeroService {
+  return new HeroService(http, logger, false);
+}
 
 export let heroServiceProvider: FactoryProvider = {
-    provide: HeroService,
-    useFactory: heroServiceFactory,
-    deps: [Http, Logger]
+  provide: HeroService,
+  useFactory: heroServiceFactory,
+  deps: [Http, Logger]
 };
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,29 +1,32 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import {Component, OnInit, Inject} from '@angular/core';
 
-import { Hero } from '../core/hero';
-import { HeroListService } from '../core/hero-list.service';
-import { HERO_LIST_SERVICE } from '../core/hero.service';
+import {Hero} from '../core/hero';
+import {HeroListService} from '../core/hero-list.service';
+import {HERO_LIST_SERVICE} from '../core/hero.service';
 
 @Component({
-    selector: 'sho-dashboard',
-    templateUrl: './dashboard.component.html',
-    styleUrls: ['./dashboard.component.css']
+  selector: 'sho-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-    heroes: Hero[];
-    highlightedHero: Hero = null;
+  heroes: Hero[];
+  highlightedHero: Hero = null;
+  heroesCount?: number = null;
+  foundHeroes: Hero[] = null;
 
-    constructor( @Inject(HERO_LIST_SERVICE) private heroService: HeroListService) { }
+  constructor(@Inject(HERO_LIST_SERVICE) private heroService: HeroListService) {
+  }
 
-    ngOnInit(): void {
-        this.initializeTopHeroes();
-    }
+  ngOnInit(): void {
+    this.initializeTopHeroes();
+  }
 
-    highlightHero(hero?: Hero): void {
-        this.highlightedHero = hero;
-    }
+  highlightHero(hero?: Hero): void {
+    this.highlightedHero = hero;
+  }
 
-    private initializeTopHeroes(): void {
-        this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1, 5));
-    }
+  private initializeTopHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes.slice(1, 5));
+  }
 }
